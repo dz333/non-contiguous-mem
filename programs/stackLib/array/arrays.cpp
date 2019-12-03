00bfea3b12a528f4e8e7bba8276e373f23f09443
dz333 <dzag@cs.cornell.edu>
2019-12-03 16:04:44 -0500

    edited to support 2 level page tables

--- programs/stackLib/array/arrays.cpp
+++ programs/stackLib/array/arrays.cpp
@@ -1,92 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "arrays.h"
 
-#define PAGE_SIZE 4096
-#define NUM_PTRS (PAGE_SIZE / sizeof(void*))
-#define NUM_ELEMS (PAGE_SIZE / sizeof(T))
+typedef struct OptionData_ {
+  float s;          // spot price
+  float strike;     // strike price
+  float r;          // risk-free interest rate
+  float divq;       // dividend rate
+  float v;          // volatility
+  float t;          // time to maturity or option expiration in years
+  //     (1yr = 1.0, 6mos = 0.5, 3mos = 0.25, ..., etc)
+  char OptionType;   // Option type.  "P"=PUT, "C"=CALL
+  float divs;       // dividend vals (not used in this test)
+  float DGrefval;   // DerivaGem Reference Value
+} OptionData;
 
-template <typename T>
-class Array
-{
-private:
-  T* ptable[NUM_PTRS];
-  bool single;
-  
-public:
-  Array(size_t size);
-  T get(int index);
-  void set(int index, T val);
-  T* getAddr(int index);
-};
-
-template <typename T>
-Array<T>::Array(size_t size)
-{
-  int numpages = ((size * sizeof(T)) / PAGE_SIZE) + 1;
-  printf("Size of element is %lu\n", sizeof(T));
-  printf("Num pages allocated is %d\n", numpages);
-  if (numpages > NUM_PTRS) {
-    printf("Cant support that many pages!!\n");
-    exit(1);
-  }
-  if (numpages > 1) {
-    single = false;
-    for (int i = 0; i < numpages; i++) {
-      T* page = (T*) malloc(PAGE_SIZE);
-      ptable[i] = page;
-    }
-  } else {
-    single = true;
-  }
-}
-
-template <typename T>
-void Array<T>::set(int index, T val)
-{
-  if (single) {
-    T *entries = (T*) ptable;
-    entries[index] = val;
-  } else {
-    int pageno = index / NUM_ELEMS;
-    int offset = index % NUM_ELEMS;
-    T *page = ptable[pageno];
-    page[offset] = val;
-  }
-}
-
-template <typename T>
-T Array<T>::get(int index) {
-  if (single) {
-    T *entries = (T*) ptable;
-    return entries[index];
-  } else {
-    int pageno = index / NUM_ELEMS;
-    int offset = index % NUM_ELEMS;
-    T *page = ptable[pageno];
-    return page[offset];
-  }
-}
-
-template <typename T>
-T* Array<T>::getAddr(int index) {
-  if (single) {
-    T *entries = (T*) ptable;
-    return &(entries[index]);
-  } else {
-    int pageno = index / NUM_ELEMS;
-    int offset = index % NUM_ELEMS;
-    return &(ptable[pageno][offset]);
-  }
-}
 
 int main()
 {
-  Array<int> a(2000000);
-  a.set(1800, 3);
-  a.set(1801, 4);
-  int x = a.get(1800);
+  Array<int> a(1000000);
+  a.set(0, 3);
+  a.set(1, 4);
+  int x = a.get(0);
   printf("Output is %d\n", x);
-  int* y = a.getAddr(1800);
+  int* y = a.getAddr(0);
   printf("Output is %d\n", *y);
   printf("Next Output is %d\n", y[1]);
+  a.set(1024, 7);
+  printf("Output is %d\n", a.get(1024));
+  a.set(524288, 12);
+  printf("Output is %d\n", a.get(524288));
+  a.set(999999, -3);
+  printf("Output is %d\n", a.get(999999));
 }
---
+++ programs/stackLib/array/arrays.h
@@ -0,0 +1,151 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PAGE_SIZE 4096
+#define PTRS_PER_PAGE (PAGE_SIZE / sizeof(void*))
+#define MAX_PAGES (PTRS_PER_PAGE * PTRS_PER_PAGE)
+#define NUM_ELEMS (PAGE_SIZE / sizeof(T))
+#define ELEMS_PER_L2 (PTRS_PER_PAGE * NUM_ELEMS)
+#define single (num_data_pages == 1)
+#define two_level (num_l1_pages > 1)
+
+#define getL1Offset(i) (i / ELEMS_PER_L2)
+#define getL2Index(i)  ((i / NUM_ELEMS) % PTRS_PER_PAGE)
+#define getL2Offset(i) (i % NUM_ELEMS)
+
+
+template <typename T>
+class Array
+{
+private:
+  void* ptable[PTRS_PER_PAGE];
+  size_t num_l1_pages;
+  size_t num_data_pages;
+  
+public:
+  Array(size_t size);
+  ~Array();
+  T get(int index);
+  void set(int index, T val);
+  T* getAddr(int index);
+};
+
+template <typename T>
+Array<T>::Array(size_t size)
+{
+  num_data_pages = ((size * sizeof(T)) / PAGE_SIZE) + 1;
+  num_l1_pages = num_data_pages / PTRS_PER_PAGE + 1;
+  printf("Size of element is %lu\n", sizeof(T));
+  printf("Num pages allocated is %lu\n", num_data_pages);
+  printf("Number of l1 pages is %lu\n", num_l1_pages);
+  if (num_data_pages > MAX_PAGES) {
+    printf("Cant support that many pages!!\n");
+    exit(1);
+  }
+  if (num_data_pages > 1) {
+    if (two_level) {
+      for (int i = 0; i < num_l1_pages; i++) {
+	void* l1_page = malloc(PAGE_SIZE);
+	ptable[i] = l1_page;
+	for (int j = 0; j < PTRS_PER_PAGE; j++) {
+	  void **l1_page_cast = (void **)l1_page;
+	  l1_page_cast[j] = malloc(PAGE_SIZE);
+	}
+      }
+    } else {
+      for (int i = 0; i < num_data_pages; i++) {
+	void* page = malloc(PAGE_SIZE);
+	ptable[i] = page;
+      }
+    }
+  }
+}
+
+template <typename T>
+Array<T>::~Array() {
+  if (!single) {
+    if (two_level) {
+      for (int i = 0; i < num_l1_pages; i++) {
+	void **l1_page = (void**) ptable[i];
+	for (int j = 0; j < PTRS_PER_PAGE; j++) {
+	  free(l1_page[j]);
+	}
+	free(l1_page);
+      }
+    } else {
+      for (int i = 0; i < num_data_pages; i++) {
+	free(ptable[i]);
+      }
+    }
+  }
+}
+
+template <typename T>
+void Array<T>::set(int index, T val)
+{
+  if (single) {
+    T *entries = (T*) ptable;
+    entries[index] = val;
+  } else if (two_level) {
+    T ***entries = (T***) ptable;
+    int l1off = getL1Offset(index);
+    int pageno = getL2Index(index);
+    int offset = getL2Offset(index);
+    T **l1_page = entries[l1off];
+    T *page = l1_page[pageno];
+    page[offset] = val;
+  } else {
+    T **entries = (T**) ptable;
+    int pageno = getL2Index(index);
+    int offset = getL2Offset(index);
+    T *page = entries[pageno];
+    page[offset] = val;
+  }
+}
+
+template <typename T>
+T Array<T>::get(int index) {
+  if (single) {
+    T *entries = (T*) ptable;
+    return entries[index];
+  } else if (two_level) {
+    T ***entries = (T***) ptable;
+    int l1off = getL1Offset(index);
+    T **l1_page = entries[l1off];
+    int pageno = getL2Index(index);
+    int offset = getL2Offset(index);
+    T *page = l1_page[pageno];
+    return page[offset];
+  } else {
+    T **entries = (T**) ptable;
+    int pageno = index / NUM_ELEMS;
+    int offset = index % NUM_ELEMS;
+    T *page = entries[pageno];
+    return page[offset];
+  }
+}
+
+template <typename T>
+T* Array<T>::getAddr(int index) {
+  if (single) {
+    T *entries = (T*) ptable;
+    return &(entries[index]);
+  } else if (two_level) {
+    T ***entries = (T***) ptable;
+    int l1off = getL1Offset(index);
+    T **l1_page = entries[l1off];
+    int pageno = getL2Index(index);
+    int offset = getL2Offset(index);
+    T *page = l1_page[pageno];
+    return &(page[offset]);
+  } else {
+    T **entries = (T**) ptable;
+    int pageno = index / NUM_ELEMS;
+    int offset = index % NUM_ELEMS;
+    return &(entries[pageno][offset]);
+  }
+}
+
+#endif
