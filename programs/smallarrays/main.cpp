14c1113adf88ac0191912c38d2be546d8618c2bd
dz333 <dzag@cs.cornell.edu>
2020-01-31 17:56:32 -0500

    updated microbenchmark code and makefile and run script

--- programs/smallarrays/main.cpp
+++ programs/smallarrays/main.cpp
@@ -2,10 +2,13 @@
 #include <cstdio>
 #include "../stackLib/array/arrays.h"
 
+#ifdef ARRAYOBJ
+Array<int> *arr;
+#endif
+
 #ifndef ARRAYOBJ
 int doWork(int* vals, int size, int iterations) {
 #else
-  Array<int> *arr;
 int doWork(Array<int> vals, int size, int iterations) {
 #endif
   for (int i = 0; i < size; i++) {
@@ -18,15 +21,83 @@ int doWork(Array<int> vals, int size, int iterations) {
   return sum;
 }
 
+
+#ifndef ARRAYOBJ
+int doScan(int* vals, int size, int iterations) {
+
+  for (int i = 0; i < size; i++) {
+    vals[i] = (i + 1) * 1734;
+  }
+  int sum = 0;
+  int *cursor = vals;
+  int *end = cursor + size;
+  for (int i = 0; i < iterations; i++) {
+    if (cursor == end) {
+      cursor = vals;
+    }
+    sum += *cursor;
+    cursor++;
+  }
+  return sum;
+}
+#else
+int doScan(Array<int> vals, int size, int iterations) {
+
+  for (int i = 0; i < size; i++) {
+    vals[i] = (i + 1) * 1734;
+  }
+  int sum = 0;
+  int idx = 0;
+  for (int i = 0; i < iterations; i++) {
+    if (idx == size) {
+      idx = 0;
+    }
+    sum += vals[idx];
+    idx++;
+  }
+  return sum;
+}
+#endif
+ 
+int doOptScan(Array<int> vals, int size, int iterations) {
+   for (int i = 0; i < size; i++) {
+    vals[i] = (i + 1) * 1734;
+  }
+  int sum = 0;
+  MemRegion<int> r = vals.getRegion(0);
+  for (int i = 0; i < iterations; i++) {
+    if(r.minValue > r.maxValue) {
+      r = vals.getRegion(i % size);
+    }
+    sum += *(r.minValue);
+    r.minValue++;
+  }
+  return sum;
+}
+
+#ifndef SCAN
+  #define FUNC doWork
+#else
+  #ifdef OPT
+    #define FUNC doOptScan
+  #else
+    #define FUNC doScan
+  #endif
+#endif
 int main(int argc, char **argv) {
   int n = argc > 1 ? atol(argv[1]) : 0;
   int m = argc > 2 ? atol(argv[2]) : 1000000;
 #ifndef ARRAYOBJ
   int* x = (int*) malloc(n * sizeof(int));
-  return doWork(x, n, m);
+  int result = FUNC(x, n, m);
+  //  printf("Result is %d\n", result);
+  free(x);
+  return  result;
 #else
   arr = new Array<int>(n);
-  return doWork(*arr, n, m);
+  int result = FUNC(*arr, n, m);
+  // printf("Result is %d\n", result);
+  return result;
 #endif
 }
 
--- programs/smallarrays/run_test.sh
+++ programs/smallarrays/run_test.sh
@@ -11,15 +11,16 @@ cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses 
 echo "Using custom Arrays"
 cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./scan_array 1000 10000000000
 
-echo "Running Pointer Chase With Small Arrays (n=1000)"
+echo "Using optimized Arrays"
+cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./scan_opt 1000 10000000000
 
-echo "Using default malloc"
-cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase 1000 10000000000
+# echo "Running Pointer Chase With Small Arrays (n=1000)"
 
-echo "Using custom Arrays"
-cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase_array 1000 10000000000
+# echo "Using default malloc"
+# cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase 1000 10000000000
 
-
+# echo "Using custom Arrays"
+# cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase_array 1000 10000000000
 
 echo "Running Linear Scan With Large Arrays (n=1000000)"
 
@@ -29,10 +30,13 @@ cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses 
 echo "Using custom Arrays"
 cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./scan_array 1000000 10000000000
 
-echo "Running Pointer Chase With Large Arrays (n=1000000)"
+echo "Using optimized Arrays"
+cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./scan_opt 1000000 10000000000
 
-echo "Using default malloc"
-cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase 1000000 10000000000
+# echo "Running Pointer Chase With Large Arrays (n=1000000)"
 
-echo "Using custom Arrays"
-cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase_array 1000000 10000000000
+# echo "Using default malloc"
+# cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase 1000000 10000000000
+
+# echo "Using custom Arrays"
+# cset shield --exec -- perf stat -r 10 -e cycles -e instructions -e cache-misses -e cache-references ./chase_array 1000000 10000000000
