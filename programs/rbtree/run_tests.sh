2ab5f7fb850aa33d7f142d4c61f3a1ce6651fd2a
dz333 <dzag@cs.cornell.edu>
2020-05-10 22:30:35 -0400

    and tests

--- programs/rbtree/run_tests.sh
+++ programs/rbtree/run_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for i in {28..28}
+#sizes=(93 95000 97000000 195000000 390000000)
+sizes=(366048349)
+for i in "${sizes[@]}"
 do
-    echo "Tree size is: $((44 * $((1 << $i))))"
-    cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 0
+    echo "Tree size is: $((44 * $i))"
+#    cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 0
     cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 5
-    echo "Now using Hugepages"
-    LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 0
-    LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 5
+     echo "Now using Hugepages"
+ #    LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 0
+#     LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes cset shield --exec -- perf stat -r 1 -e cycles,instructions,L1-dcache-load-misses,L1-dcache-loads,dTLB-load-misses,dTLB-loads,faults,dtlb_load_misses.walk_active ./rb $i 5
 done
